18118c556f2ab5a94d3bdfba05b22713d207b91c
pamkaya-dev <tharakadilshan4433@gmail.com>
2026-01-11 09:15:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 // Preloader
-window.addEventListener('load', () => {
-  const pre = document.getElementById('preloader');
-  const content = document.getElementById('content');
-
-  pre.style.opacity = '0';
+window.addEventListener("load", () => {
+  const pre = document.getElementById("preloader");
+  const main = document.getElementById("mainContent");
+  pre.style.opacity = "0";
   setTimeout(() => {
     pre.remove();
-    content.style.opacity = '1';
-  }, 700);
+    main.style.opacity = "1";
+  }, 800);
 });
 
-// Sidebar Toggle
-const menuBtn = document.getElementById('menuBtn');
-const sidebar = document.getElementById('sidebar');
+// Sidebar toggle
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
 
 menuBtn.onclick = () => {
-  sidebar.style.left = '0';
-  menuBtn.style.opacity = '0';
+  sidebar.style.left = "0";
+  menuBtn.style.opacity = "0";
 };
 
-document.addEventListener('click', e => {
+document.addEventListener("click", e => {
   if (!sidebar.contains(e.target) && !menuBtn.contains(e.target)) {
-    sidebar.style.left = '-280px';
-    menuBtn.style.opacity = '1';
+    sidebar.style.left = "-280px";
+    menuBtn.style.opacity = "1";
   }
 });
